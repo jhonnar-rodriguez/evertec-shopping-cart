9b683928d3e4a3a1e4388c9a586df1d92ff60202
Jhonnar Rodriguez <jhonnar.rodriguez@gmail.com>
2020-07-25 08:53:57 -0500

    User migration Changed :zap:
    New fields were added in order to apply the desired logic.

--- shopping-cart-api/database/migrations/2014_10_12_000000_create_users_table.php
+++ shopping-cart-api/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('first_name', 50 );
+            $table->string('last_name', 50 );
+            $table->string('email' )->unique();
+            $table->string('password' );
+            $table->string('phone_number', 15 );
+            $table->tinyInteger('active' )->default(true )->unsigned();
             $table->rememberToken();
             $table->timestamps();
         });
